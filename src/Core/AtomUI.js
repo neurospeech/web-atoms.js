bd9cca76d07f9c1aca3c3a5e860f0add26d6b071
ackava <ackava@gmail.com>
2015-07-31 18:35:32 +0530

    wip

--- src/Core/AtomUI.js
+++ src/Core/AtomUI.js
@@ -403,13 +403,22 @@ AtomUI.isIE7 = window.navigator.userAgent.indexOf("MSIE 7.0") != -1;
 AtomUI.isIE8 = window.navigator.userAgent.indexOf("MSIE 8.0") != -1;
 
 window.AtomUri = function (url) {
-    var path, query, hash;
+    var path;
+    var query = "";
+    var hash = "";
     var t = url.split('?');
     path = t[0];
-    query = t[1] || "";
-    t = query.split('#');
-    query = t[0];
-    hash = t[1] || "";
+    if (t.length == 2) {
+        query = t[1] || "";
+
+        t = query.split('#');
+        query = t[0];
+        hash = t[1] || "";
+    } else {
+        t = path.split('#');
+        path = t[0];
+        hash = t[1] || "";
+    }
 
     // extract protocol and domain...
 
